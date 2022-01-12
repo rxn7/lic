426e764e6bb8febf8abbe093660de3d8e4ba0730
rxtthin <rotthin_dev@protonmail.com>
2022-01-12 12:33:32 +0100

    Print total lines only if multiple files has been passed

--- lic.c
+++ lic.c
@@ -5,7 +5,7 @@ static unsigned long count_lines(const char *file_path) {
 	FILE *f = fopen(file_path, "r");
 	if(!f) {
 		fprintf(stderr, "Failed to open file `%s`.\n", file_path);
-		exit(-1);
+		return 0;
 	}
 
 	unsigned long count = 0;
@@ -24,11 +24,15 @@ static unsigned long count_lines(const char *file_path) {
 int main(int argc, const char **argv) {
         unsigned int  total = 0; 
 	unsigned long count;
-        for(unsigned int i=1; i<argc; ++i) {
+	unsigned int i = 1;
+	
+        for(; i<argc; ++i) {
 		count = count_lines(argv[i]);
 		total += count;
 		printf("%s: %lu\n", argv[i], count);
 	}
 
-        printf("\ntotal lines: %u\n", total);
+	if(i > 2) {
+		printf("\ntotal lines: %u\n", total);
+	}
 }
